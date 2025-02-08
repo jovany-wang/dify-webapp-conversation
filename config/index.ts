1ac405d8cb58b0bd36e0616d3c54b8aea24e34ec
jovany.wq <jovany.wq@antgroup.com>
2025-02-08 21:57:41 +0800

    1

--- config/index.ts
+++ config/index.ts
@@ -3,9 +3,9 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'UNFIELD AI',
+  title: '未知AI(deepseek-R1)',
   description: '未知领域',
-  copyright: '未知领域@2025',
+  copyright: '未知领域科技有限公司',
   privacy_policy: '',
   default_language: 'zh',
 }
